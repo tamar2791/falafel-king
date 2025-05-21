5007455f4ce151fdf9296c5a560c362799507cdf
tamar2791 <tamizaltz@gmail.com>
2025-05-21 21:41:38 +0300

    update home

--- home.js
+++ home.js
@@ -1,26 +1,15 @@
-document.querySelector("#h").addEventListener("click",ho)
-        document.querySelector("#x").addEventListener("click",xx)
-        document.querySelector("#k").addEventListener("click",kk)
-        function kk(){
-            document.querySelector("#kredit").style.display="block"
-            document.querySelector("#x").style.display="block"
-            document.querySelector("#h").removeEventListener("click",ho)
-            document.querySelector("#k").removeEventListener("click",kk)
-            document.querySelectorAll("a")[0].style.display="none"
-        }
-        function ho() {
-            document.querySelector("#d").style.display="block"
-            document.querySelector("#x").style.display="block"
-            document.querySelector("#h").removeEventListener("click",ho)
-            document.querySelector("#k").removeEventListener("click",kk)
-            document.querySelectorAll("a")[0].style.display="none"
-        }
-        function xx() {
-            document.querySelector("#d").style.display="none"
-            document.querySelector("#kredit").style.display="none"
-            document.querySelector("#x").style.display="none"
-            document.querySelector("#h").addEventListener("click",ho)
-            document.querySelector("#x").addEventListener("click",xx)
-            document.querySelector("#k").addEventListener("click",kk)
-            document.querySelectorAll("a")[0].style.display="block"
-        }
+document.querySelector("#ins").addEventListener("click", ho)
+document.querySelector("#x").addEventListener("click", xx)
+function ho() {
+    document.querySelector("#ins_page").style.display = "block"
+    document.querySelector("#x").style.display = "block"
+    document.querySelector("#ins").removeEventListener("click", ho)
+    document.querySelectorAll("a")[0].style.display = "none"
+}
+function xx() {
+    document.querySelector("#ins_page").style.display = "none"
+    document.querySelector("#x").style.display = "none"
+    document.querySelector("#ins").addEventListener("click", ho)
+    document.querySelector("#x").addEventListener("click", xx)
+    document.querySelectorAll("a")[0].style.display = "block"
+}
